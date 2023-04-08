daa897c23c3bc2896a0b2f5ee1f1989d78f0232e
Shahria Abeed <02sabeed@gmail.com>
2023-04-08 17:25:35 -0400

    INSERT statements for criminal & officer

--- UPDATES.sql
+++ UPDATES.sql
@@ -1,1 +1,26 @@
--- fill tables with data
+INSERT INTO criminal (c_id, c_name,c_address, c_phone_num, c_off_status, c_prob_status)
+VALUES 
+    (1, "John Doe", "123 Main Street", 646639677, "yes", "yes"),
+    (2, "John Smith", "346 South Street", 917456283, "no", "yes"),
+    (3, "Edgar Allen", "346 Main Street", 968152736, "no", "yes"),
+    (4, "Ty Johnson", "197 West Avenue", 285013794, "no", "yes"),
+    (5, "Matt Nelson", "587 Main Street", 752863014, "yes", "no"),
+    (6, "Alex Johnson", "95 East Road", 369824175, "no", "yes"),
+    (7, "Rodrick Davis", "45 Red Place", 605281437, "yes", "no"),
+    (8, "Daniel Lee", "23 Green Street", 913476028, "no", "no"),
+    (9, "Christopher Scott", "89 South Road", 482096357, "yes", "yes"),
+    (10,"Ricky Richard", "123 Red Place", 194865230, "no", "no");
+
+
+INSERT INTO officer (badge_number ,o_name ,o_precinct ,o_phone_number , o_status)
+VALUES 
+    (1, "Michael Johnson", 35, 326214789, "active"),
+    (2, "William Davis", 35, 878639041, "active"),
+    (3, "David Wilson", 35, 165208037, "active"),
+    (4, "James Thompson", 35, 427886124, "inactive"),
+    (5, "Andrew Parker", 47, 772907091, "inactive"),
+    (6, "Christopher Mitchell", 47, 462672136, "active"),
+    (7, "Brian Baker", 47, 377794644, "active"),
+    (8, "Robert Turner", 47, 129023868, "inactive"),
+    (9, "Kevin Carter", 29, 573394225, "active"),
+    (10, "Matthew King", 29, 596366876, "inactive");
