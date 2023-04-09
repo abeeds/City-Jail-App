d466019482229306d9265ad6699ccba560eb0a64
gzhao10 <68718446+gzhao10@users.noreply.github.com>
2023-04-09 18:24:20 -0400

    Update COMMANDS.sql

--- COMMANDS.sql
+++ COMMANDS.sql
@@ -1,13 +1,37 @@
--- some ideas for functions:
---
--- searchByCity(city):
--- table showing ( c_last, c_first, c_city, c_state, code_desc,
---                start_date, end_date)
+DELIMITER $$
 
--- searchByZipCode(zipcode)
--- same as above
+--Search for criminal info by city
+CREATE OR REPLACE PROCEDURE searchByCity(city VARCHAR(64))
+BEGIN
+	SELECT c_last, c_first, c_city, c_state, code_desc, start_date, end_date
+    FROM criminal, sentence
+    WHERE criminal.c_id = sentence.c_id
+    AND c_city = city;
+END$$
 
--- makePayment(amount)
--- amount_paid += amount
 
---
+--Search for criminal info by zipcode
+CREATE OR REPLACE PROCEDURE searchByZipCode(zipcode VARCHAR(64))
+BEGIN
+	SELECT c_last, c_first, c_city, c_state, code_desc, start_date, end_date
+    FROM criminal, sentence
+    WHERE criminal.c_id = sentence.c_id
+    AND c_zip = zip;
+END$$
+
+--Count number of active officers
+CREATE OR REPLACE FUNCTION count_officers() RETURNS INT
+BEGIN
+  RETURN (SELECT COUNT (badge_number)
+          FROM officer
+          WHERE o_status = 'a');
+END$$
+
+--Make a payment
+CREATE OR REPLACE PROCEDURE make_payment(crimeid INT, chargeid INT, payment DECIMAL, paydate DATE)
+BEGIN
+  UPDATE charge
+  SET amount_paid = amount_paid + payment, payment_date = paydate
+  WHERE case_id = crimeid
+  AND charge_id = chargeid;
+END$$
