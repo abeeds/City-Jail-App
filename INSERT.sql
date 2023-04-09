c3eba2724b2443130e94d06126335071437396dd
Shahria Abeed <shahriaabeed@gmail.com>
2023-04-08 23:08:01 -0400

    ADDED INSERT INTO prob_officer

--- INSERT.sql
+++ INSERT.sql
@@ -45,3 +45,22 @@ VALUES
     (8, 8, 'f', '2009-05-22', 'c', '2009-07-01', '2009-07-15'),
     (9, 9, 'm', '2012-10-04', 'ca', '2013-01-01', '2013-01-15'),
     (10, 10, 'o', '2015-03-18', 'ia', '2015-06-01', '2015-06-15');
+
+
+-- p_status:
+-- i: inactive
+-- a: active
+INSERT INTO prob_officer
+    (p_id, p_last, p_first, p_phone_number, p_street, p_city, p_state, p_zip, p_email, p_status)
+VALUES 
+    (1, 'Johnson', 'David', 541789623, '123 Main St', 'New York', 'NY', 10001, 'david.johnson@gmail.com', 'a'),
+    (2, 'Lee', 'Karen', 789123456, '456 Elm St', 'Philadelphia', 'PA', 19102, 'karen.lee@gmail.com', 'i'),
+    (3, 'Garcia', 'Luis', 302456789, '789 Oak Ave', 'Miami', 'FL', 33131, 'luis.garcia@gmail.com', 'a'),
+    (4, 'Nguyen', 'Anna', 654321987, '567 Pine St', 'New York', 'NY', 10005, 'anna.nguyen@gmail.com', 'i'),
+    (5, 'Wang', 'Kevin', 987654321, '890 Maple Ave', 'Philadelphia', 'PA', 19103, 'kevin.wang@gmail.com', 'a'),
+    (6, 'Gonzalez', 'Maria', 123456789, '123 Elm St', 'Miami', 'FL', 33133, 'maria.gonzalez@gmail.com', 'i'),
+    (7, 'Adams', 'John', 987654321, '456 Oak St', 'New York', 'NY', 10010, 'john.smith@gmail.com', 'a'),
+    (8, 'Jones', 'Emma', 123456789, '789 Pine St', 'Philadelphia', 'PA', 19104, 'emma.jones@gmail.com', 'i'),
+    (9, 'Washington', 'George', 456789123, '234 Maple Ave', 'Miami', 'FL', 33134, 'michael.brown@gmail.com', 'a'),
+    (10, 'Davis', 'Sarah', 789123456, '567 Elm St', 'New York', 'NY', 10020, 'sarah.davis@gmail.com', 'i');
+
