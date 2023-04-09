9603fb6c37833ab388c9c69e2a7895e6135ab37c
Shahria Abeed <02sabeed@gmail.com>
2023-04-08 22:29:17 -0400

    Split address in Create.sql+added insert criminal

--- INSERT.sql
+++ INSERT.sql
@@ -1,26 +1,13 @@
-INSERT INTO criminal (c_id, c_name,c_address, c_phone_num, c_off_status, c_prob_status)
+INSERT INTO criminal 
+    (c_id, c_last, c_first, c_street, c_city, c_state, c_zip, c_phone_num, V_status, P_status)
 VALUES 
-    (1, "John Doe", "123 Main Street", 646639677, "yes", "yes"),
-    (2, "John Smith", "346 South Street", 917456283, "no", "yes"),
-    (3, "Edgar Allen", "346 Main Street", 968152736, "no", "yes"),
-    (4, "Ty Johnson", "197 West Avenue", 285013794, "no", "yes"),
-    (5, "Matt Nelson", "587 Main Street", 752863014, "yes", "no"),
-    (6, "Alex Johnson", "95 East Road", 369824175, "no", "yes"),
-    (7, "Rodrick Davis", "45 Red Place", 605281437, "yes", "no"),
-    (8, "Daniel Lee", "23 Green Street", 913476028, "no", "no"),
-    (9, "Christopher Scott", "89 South Road", 482096357, "yes", "yes"),
-    (10,"Ricky Richard", "123 Red Place", 194865230, "no", "no");
-
-
-INSERT INTO officer (badge_number ,o_name ,o_precinct ,o_phone_number , o_status)
-VALUES 
-    (1, "Michael Johnson", 35, 326214789, "active"),
-    (2, "William Davis", 35, 878639041, "active"),
-    (3, "David Wilson", 35, 165208037, "active"),
-    (4, "James Thompson", 35, 427886124, "inactive"),
-    (5, "Andrew Parker", 47, 772907091, "inactive"),
-    (6, "Christopher Mitchell", 47, 462672136, "active"),
-    (7, "Brian Baker", 47, 377794644, "active"),
-    (8, "Robert Turner", 47, 129023868, "inactive"),
-    (9, "Kevin Carter", 29, 573394225, "active"),
-    (10, "Matthew King", 29, 596366876, "inactive");
+    (1, 'Smith', 'John', '123 Main St', 'New York', 'NY', 12345, 123456789, 'y', 'n'),
+    (2, 'Johnson', 'Edward', '456 Oak St', 'Flushing', 'NY', 15485, 234567890, 'n', 'y'),
+    (3, 'Leonard', 'Mike', '789 Maple Ave', 'Jamaica', 'NY', 12923, 345678901, 'y', 'y'),
+    (4, 'Gibson', 'Carlos', '321 Elm St', 'Astoria', 'NY', 98765, 456789012, 'n', 'n'),
+    (5, 'Kim', 'Jeremy', '654 Pine St', 'Newark', 'NJ', 43210, 567890123, 'y', 'y'),
+    (6, 'Joseph', 'David', '987 Cedar Blvd', 'Trenton', 'NJ', 56789, 678901234, 'n', 'y'),
+    (7, 'Wilson', 'Gary', '246 Walnut Ave', 'Philadelphia', 'PA', 75421, 789012345, 'y', 'n'),
+    (8, 'Doe', 'Anthony', '135 Oak St', 'Pittsburgh', 'PA', 23532, 890123456, 'n', 'n'),
+    (9, 'Nelson', 'Mike', '864 Elm St', 'Orlando', 'FL', 91293, 901234567, 'y', 'y'),
+    (10, 'Martin', 'Andy', '123 Pine St', 'Miami', 'FL', 33542, 789012345, 'n', 'y');
