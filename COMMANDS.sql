02342510c06b48d27eb767bc7e998b1453319593
Shahria Abeed <shahriaabeed@gmail.com>
2023-04-09 01:24:39 -0400

    Finished Inserts

---
+++ COMMANDS.sql
@@ -0,0 +1,13 @@
+-- some ideas for functions:
+--
+-- searchByCity(city):
+-- table showing ( c_last, c_first, c_city, c_state, code_desc,
+--                start_date, end_date)
+
+-- searchByZipCode(zipcode)
+-- same as above
+
+-- makePayment(amount)
+-- amount_paid += amount
+
+--
