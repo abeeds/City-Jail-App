1953d1404f8d6c5f2edf853a097a757195c24d50
Shahria Abeed <shahriaabeed@gmail.com>
2023-04-24 17:20:17 -0400

    Some form validation

--- Web App/User/db-functions.php
+++ Web App/User/db-functions.php
@@ -2,13 +2,15 @@
 
 // safety feature
 // removes possibility of typing in SQL code into the form
-function removeInvalid(&$string){
+// ignore leading and trailing whitespace
+function formatInput(&$string){
     $INVALID_CHARS =
         [",", "?", "!", ":", ";",
          "+", "<", ">", "%", "~",
         "€", "$", "[", "]", "{", "}", "(", ")",
         "@", "&", "#", "*", "„", "'", '"'];
     $string = str_replace($INVALID_CHARS, "", $string);
+    $string = trim($string, " ");
 }
 
 function connectToDB_guest() {
@@ -25,85 +27,88 @@ function connectToDB_guest() {
 }
 
 function makeTable_criminal($name, $city, $state, $zip, $database=NULL) {
-    // making upper most row with labels
-    removeInvalid($name);
-    removeInvalid($city);
-    removeInvalid($state);
+    if(!$database) {
+        echo "<p> Failed to connect to database. </p>";
+        return;
+    }
 
-    echo "<p> Showing Results For: </p>";
+    // making upper most row with labels
+    formatInput($name);
+    formatInput($city);
+    formatInput($state);
+    echo "<p> Showing Results For: <br>";
     if($name !== "") {
-        echo "<p> Name: " . $name . "</p>";
+        echo "Name: " . $name . "<br>";
     }
     if($city !== "") {
-        echo "<p> City: " . $city . "</p>";
+        echo "City: " . $city . "<br>";
     }
     if($state !== "") {
-        echo "<p> State: " . $state . "</p>";
+        echo "State: " . $state . "<br>";
     }
-    if($zip !== "") {    
-        echo "<p> Zip Code: " . $zip . "</p>";
+    if($zip !== "") {
+        echo "Zip Code: " . $zip . "<br>";
     }
 
-    echo 
-    "<table id=\"Criminals\">
-    <!-- Row with labels for each column -->
-        <tr class=\"row-labels\">
-        <th>ID</th>
-        <th>Last</th>
-        <th>First Name</th>
-        <th>City</th>
-        <th>State</th>
-        <th>Zip Code</th>
-        <th>Violent Offender Status</th>
-        <th>Probation Status</th>
-        </tr>";
+    echo "</p>";
 
-    // only form query if connected to database
-    if($database !== NULL) {
-        $aQuery = "SELECT * FROM criminal c";
-        
+    // row with labels
+    echo    "<table id=\"Criminals\">
+                <tr class=\"row-labels\">
+                    <th>ID</th>
+                    <th>Last</th>
+                    <th>First Name</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zip Code</th>
+                    <th>Violent Offender Status</th>
+                    <th>Probation Status</th>
+                </tr>";
+    $aQuery = "SELECT * FROM criminal c";
+    
 
-        // add WHERE clause as long as one field is present
-        if($name !== "" || $city !== "" || $state !== "" || $zip !== "") {
-            $aQuery .= " WHERE ";
-            $aQuery .= "CONCAT(c.c_first, ' ',  c.c_last) LIKE '%" . $name . "%' AND ";
-            $aQuery .= "c.c_city LIKE '%" . $city . "%' " ;
-            $aQuery .= " AND c.c_state LIKE '%" . $state . "%'" ;
-            if($zip !== -1) {
-                $aQuery .= " AND c.c_zip = " . $zip;
-            }
-            $aQuery .= ";";
+    // add WHERE clause as long as one field is present
+    if($name !== "" || $city !== "" || $state !== "" || $zip !== "") {
+        $aQuery .= " WHERE ";
+        $aQuery .= "CONCAT(c.c_first, ' ',  c.c_last) LIKE '%" . $name . "%' AND ";
+        $aQuery .= "c.c_city LIKE '%" . $city . "%' " ;
+        $aQuery .= " AND c.c_state LIKE '%" . $state . "%'" ;
+        if($zip !== "") {
+            $aQuery .= " AND c.c_zip = " . $zip;
         }
-
-        $result = $database->query($aQuery);
-        if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<th>" . $row["c_id"] . "</th>";
-                echo "<th>" . $row["c_last"] . "</th>";
-                echo "<th>" . $row["c_first"] . "</th>";
-                echo "<th>" . $row["c_city"] . "</th>";
-                echo "<th>" . $row["c_state"] . "</th>";
-                echo "<th>" . $row["c_zip"] . "</th>";
-                if($row["V_status"] === "y") {
-                    echo "<th>" . "Yes" . "</th>";
-                }
-                else {
-                    echo "<th>" . "No" . "</th>";
-                }
-                
-                if($row["P_status"] === "y") {
-                    echo "<th>" . "Yes" . "</th>";
-                }
-                else {
-                    echo "<th>" . "No" . "</th>";
-                }
-                
-                echo "</tr>";
-            }
-        }
-
+        $aQuery .= ";";
     }
-  echo "</table>";
+
+    $result = $database->query($aQuery);
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<th>" . $row["c_id"] . "</th>";
+            echo "<th>" . $row["c_last"] . "</th>";
+            echo "<th>" . $row["c_first"] . "</th>";
+            echo "<th>" . $row["c_city"] . "</th>";
+            echo "<th>" . $row["c_state"] . "</th>";
+            echo "<th>" . $row["c_zip"] . "</th>";
+            if($row["V_status"] === "y") {
+                echo "<th>" . "Yes" . "</th>";
+            }
+            else {
+                echo "<th>" . "No" . "</th>";
+            }
+            
+            if($row["P_status"] === "y") {
+                echo "<th>" . "Yes" . "</th>";
+            }
+            else {
+                echo "<th>" . "No" . "</th>";
+            }
+            
+            echo "</tr>";
+        }
+    }
+    echo "</table>";
+    
+  
+  
 }
 ?>
