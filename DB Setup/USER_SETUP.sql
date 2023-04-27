11df057312a5eac3b847a7f35c3d715452d1f0a7
Shahria Abeed <shahriaabeed@gmail.com>
2023-04-27 19:41:06 -0400

    updated user_setup.sql

--- DB Setup/USER_SETUP.sql
+++ DB Setup/USER_SETUP.sql
@@ -6,4 +6,5 @@ GRANT SELECT ON cityjail.sentence TO 'guest'@'localhost';
 GRANT SELECT ON cityjail.appeal TO 'guest'@'localhost';
 GRANT SELECT ON cityjail.crime TO 'guest'@'localhost';
 
-
+CREATE USER 'admin123'@'localhost' IDENTIFIED BY 'admin321';
+GRANT ALL PRIVILEGES ON cityjail.* TO 'admin123'@'localhost';
