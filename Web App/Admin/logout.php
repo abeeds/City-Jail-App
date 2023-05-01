1c641d6070ae1744786e8e6c7f86254888423836
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2023-05-01 18:00:29 -0400

    fix for logout

--- Web App/Admin/logout.php
+++ Web App/Admin/logout.php
@@ -1,7 +1,8 @@
 <?php
     // force username cookie to expire
     if (isset($_COOKIE['username'])) {
-        setcookie('username', "", time() - 3600);
+        setcookie('username', "", time() - 3600, '/'); 
+        unset($_COOKIE['username']);
     }
 
     header('Location: ../User/criminals.php');
